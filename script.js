279b31026b6e400002186e48da5174ba1ac2bff6
Carlos eduardo <carlosslopes4@gmail.com>
2026-02-25 15:32:47 -0300

    Finished

--- script.js
+++ script.js
@@ -18,15 +18,11 @@ document.querySelectorAll('.item').forEach((item)=>{
 // Funções
 function itemClick(event){
     let item = event.target.getAttribute('data-item')
-    if(square[item] === ''){
+    if(playing && square[item] === ''){
         square[item] = player
         renderSquare()
         togglePlayer()
     }
-    
-    
-
-
 }
 
 function reset(){
@@ -50,6 +46,8 @@ function renderSquare(){
         let item = document.querySelector(`div[data-item=${i}]`)
         item.innerHTML = square[i]
     }
+
+    checkGame()
 }
 
 function renderInfo(){
@@ -60,4 +58,54 @@ function renderInfo(){
 function togglePlayer(){
     player = (player === 'x') ? 'o' : 'x'
     renderInfo()
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "X" venceu'
+        playing = false
+    } else if (checkWinnerFor('o')) {
+        warning = 'O "o" venceu'
+        playing = false
+    } else if (isFull()) {
+        warning = 'Empate!'
+        playing = false
+    }
+}
+
+function checkWinnerFor(player){
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+        
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1',
+
+    ]
+
+    for(let w in pos){
+        let pArray = pos[w].split(',')
+        let hasWon = pArray.every((option)=> square[option]=== player)
+        if(hasWon){
+            return true
+        }       
+
+    }
+    return false
+}
+
+function isFull(){
+    for(let i in square){
+        if(square[i] === ''){
+            return false
+        }
+    }
+    return true
+
+    
 }
